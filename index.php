50984ca25514f2c94b7638708be8cbe3766ebf40
Kasiin24 <39538268+Kasiin24@users.noreply.github.com>
2019-03-11 23:40:31 +0100

    Mejoras tras el Sprint Review 1 v2

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 
 <body>
     <div class="cuerpo">
-
         <header>
             <img src="./imgs/logo_u15.svg" alt="Logo Empresarial">
 
@@ -60,48 +59,40 @@
                     
                         ?>
                         <div class="inputConLogo"><!--  Esta es la segunda-->
-                        <img src="./imgs/lock.svg" alt="Logo Usuario" height="35px" width="35px">
-                        <input type="password" name="password" id="" placeholder="Contraseña" class="inputDato" required>
+                            <img src="./imgs/lock.svg" alt="Logo Usuario" height="35px" width="35px">
+                            <input type="password" name="password" id="" placeholder="Contraseña" class="inputDato" required>
                         </div>
                         <div id="botonesFormulario"><!--  Esta es la tercera-->
                             <a class="boton_rojo" onclick="ocultarModal()">Cancelar</a>
-                        
                             <input type="submit"name="" id="botonEnviar" class="botonEnviar">
-                        
-                      
-                        
                         </div>
-                        
-        
                     </form>
                     <div class="contacto">
-                            <p style="color:blue"><a href="">¿Has olvidado tu contraseña?</a></p>
-                        
-                        </div>
-            </div>
+                            <p style="color:blue"><a href="">¿Has olvidado tu contraseña?</a></p>    
+                    </div>
+                </div>
             </section>
 
         <!-- Parte Marc Alandete - FIN -->
 
-        <section class="carac" id="carac">
-            <article><img src="./imgs/ejGrafica-hum.PNG.png" alt="Humedad">
-                <p>La aplicación te permite visualizar las medidas que captan los sensores en tiempo real, como la humedad. De esta forma usted puede controlar mejor sus cultivos para una mejor producción.</p>
-            </article>
-            <article><img src="./imgs/ejGrafica-temp.PNG.png" alt="Temperatura">
-                <p>También permite visualizar las medidas de temperatura, que influye en el crecimiento de la planta y la productividad de las cosechas. Así, usted puede equilibrar estos factores mucho más fácil.</p>
-            </article>
-            <article><img src="./imgs/ejApp-campo.png" alt="Celular">
-                <p>Nuestras redes de sensores inalámbricas combinadas con otras tecnologías le permiten evaluar el estado de sus cultivos para un estudio de la agricultura con precisión.</p>
-            </article>
-        </section>
+            <section class="carac" id="carac">
+                <article><img src="./imgs/ejGrafica-hum.PNG.png" alt="Humedad">
+                    <p>La aplicación te permite visualizar las medidas que captan los sensores en tiempo real, como la humedad. De esta forma usted puede controlar mejor sus cultivos para una mejor producción.</p>
+                </article>
+                <article><img src="./imgs/ejGrafica-temp.PNG.png" alt="Temperatura">
+                    <p>También permite visualizar las medidas de temperatura, que influye en el crecimiento de la planta y la productividad de las cosechas. Así, usted puede equilibrar estos factores mucho más fácil.</p>
+                </article>
+                <article><img src="./imgs/ejApp-campo.png" alt="Celular">
+                    <p>Nuestras redes de sensores inalámbricas combinadas con otras tecnologías le permiten evaluar el estado de sus cultivos para un estudio de la agricultura con precisión.</p>
+                </article>
+            </section>
 
          <!-- Parte Marc Alandete -->
         
 
-        <script src="./js/modal.js"></script>
-            <script>
-                <?php 
-                    
+            <script src="./js/modal.js"></script>
+                <script>
+                    <?php  
                     if (isset($_GET['loginInvalido'])) {
                         $parametro=$_GET['loginInvalido'];
                         if ($parametro == true) {
@@ -111,24 +102,20 @@
                         }
                         unset($_GET['loginInvalido']);
                     ?>
-                
-        
             </script>
 
         <!-- Parte Marc Alandete - FIN -->
 
-        <footer>
-            <li>
-
-                <a href="#">Términos y condiciones</a>
-                <a href="#">Mapa del sitio</a>
-                <a href="#">Política de privacidad</a>
-                <a href="#">FAQ</a>
-            </li>
-            <p>©Todos los derechos reservados</p>
-        </footer>
-    </div>
-</body>
-
+            <footer>
+                <li>
+                    <a href="#">Términos y condiciones</a>
+                    <a href="#">Mapa del sitio</a>
+                    <a href="#">Política de privacidad</a>
+                    <a href="#">FAQ</a>
+                </li>
+                <p>©Todos los derechos reservados</p>
+            </footer>
+        </div>
+    </body>
 </html>
 
